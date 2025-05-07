9bf87456f48d3c3657e45adc81104c6a15f96c10
Hugo De Lelis <hugodelelis05@gmail.com>
2025-05-07 14:25:09 -0300

    chore(config) 🔧: add environment variable typings and project structure improvements

---
+++ backend/src/connection/mongo.ts
@@ -0,0 +1,12 @@
+// .env
+import 'dotenv/config';
+
+// modules
+import mongoose from "mongoose";
+
+export async function main() {
+    await mongoose.connect(process.env.DB_URI)
+    console.log('Connected on db')
+}
+
+main().catch(e => console.log(e))
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,29 @@
+// .env
 import 'dotenv/config';
 
-console.log("teste")
-console.log(process.env.TESTE)
+// modules
+import Fastify, { FastifyInstance } from 'fastify';
+import cors from '@fastify/cors';
+import fastifyStatic from '@fastify/static';
+import path from 'path';
+
+// consts
+const app: FastifyInstance = Fastify({ logger: false })
+const PORT: number = process.env.PORT
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+// configs
+await app.register(cors)
+
+await app.register(fastifyStatic, { root: path.join(__dirname, 'public'),  prefix: '/public/' })
+
+// routes
+
+
+// conn 
+try {
+    app.listen({port: PORT})
+    console.log(`Server listening on port ${PORT}`)
+} catch(e) {
+    console.log(e)
+}
